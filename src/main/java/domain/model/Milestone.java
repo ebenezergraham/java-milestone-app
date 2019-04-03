f48245560919eed0c823702b41e14419295e6329
Ebenezer Graham <egraham15@alustudent.com>
2019-04-03 23:59:18 +0400

    added details to models

--- src/main/java/domain/model/Milestone.java
+++ src/main/java/domain/model/Milestone.java
@@ -1,4 +1,48 @@
 package domain.model;
 
 public class Milestone {
+	
+	private String id;
+	private String title;
+	private String date;
+	private String description;
+	
+	public Milestone(String id, String title, String date, String description) {
+		this.id = id;
+		this.title = title;
+		this.date = date;
+		this.description = description;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getDate() {
+		return date;
+	}
+	
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
--- src/main/java/domain/model/Project.java
+++ src/main/java/domain/model/Project.java
@@ -1,4 +1,39 @@
 package domain.model;
 
+import java.util.List;
+
 public class Project {
+	private String userId;
+	private String name;
+	private List<Milestone> milestoneList;
+	
+	public Project(String userId, String name, List<Milestone> milestoneList) {
+		this.userId = userId;
+		this.name = name;
+		this.milestoneList = milestoneList;
+	}
+	
+	public String getUserId() {
+		return userId;
+	}
+	
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Milestone> getMilestoneList() {
+		return milestoneList;
+	}
+	
+	public void setMilestoneList(List<Milestone> milestoneList) {
+		this.milestoneList = milestoneList;
+	}
 }
