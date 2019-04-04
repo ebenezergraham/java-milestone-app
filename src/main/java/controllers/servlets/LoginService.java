0e41f48dc6f56a74f8d3190b04d5ea042bfa435a
Lina Bennani Dosse <ldosse15@alustudent.com>
2019-04-04 15:49:29 +0400

    deleted LoginService duplicate.

--- src/main/java/controllers/servlets/LoginService.java
+++
@@ -1,9 +0,0 @@
-package controllers.servlets;
-
-public class LoginService {
-    public boolean isUserValid(String user, String password){
-        if (user.equals("mikasa") && password.equals("snk"))
-            return true;
-        return false;
-    }
-}
