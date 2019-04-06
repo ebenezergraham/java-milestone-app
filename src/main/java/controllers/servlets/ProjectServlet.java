4151a3be65eeabbd44a6f67e24177085395cdc4c
Lina Bennani Dosse <ldosse15@alustudent.com>
2019-04-06 23:37:41 +0400

    add jstl jar to WEB-INF/lib

---
+++ src/main/java/controllers/servlets/ProjectServlet.java
@@ -0,0 +1,41 @@
+package controllers.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    private LoginService loginService = new LoginService();
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request,response);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String password = request.getParameter("password");
+        System.out.println(name);
+        boolean isUserValid = loginService.isUserValid(name, password);
+
+        if(isUserValid) {
+            request.getSession().setAttribute("name",name);
+            System.out.println(request.getSession().getAttribute("name"));
+            response.sendRedirect("/");
+        }else{
+            System.out.println("Invalid credentials");
+            String errorMessage = "Invalid credentials";
+            request.setAttribute("Invalid credentials", errorMessage);
+            request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+        }
+    }
+}
