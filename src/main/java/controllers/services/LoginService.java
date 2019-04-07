87db5e1b155715ac743af5efb165f3486c4f0093
Omeiza17 <smichael15@alustudent.com>
2019-04-07 06:14:05 +0400

    Hashing and Validation done!
    Changes the data models again.
    Proper validation implemented. Salting and Hashing  done with CSPRING and PBKDF2 respectively.

--- src/main/java/controllers/services/LoginService.java
+++ src/main/java/controllers/services/LoginService.java
@@ -1,10 +1,39 @@
 package controllers.services;
 
 import domain.model.User;
+import lombok.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import utils.PasswordHash;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class LoginService {
-    public static boolean isUserValid(String username, String password){
-        User user = UserService.getInstance().getUser();
-        return username.equals(user.getUsername()) && password.equals(user.getPassword());
+  private static final Logger LOGGER = LoggerFactory.getLogger(LoginService.class.getName());
+  private User mUser = UserService.getInstance().getUser();
+
+  public synchronized boolean login(@NonNull String username, @NonNull String password) {
+    try {
+      String storedHash = mUser.getHash();
+      System.out.println(storedHash);
+      return storedHash != null && PasswordHash.validatePassword(password, storedHash);
+    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+      LOGGER.error("Can't validate password", e.getCause());
+      return false;
     }
+  }
+
+  public synchronized boolean register(@NonNull String usename, @NonNull String password) {
+    String storedHash = mUser.getHash();
+    if (storedHash != null) return false;
+    try {
+      String newHash = PasswordHash.createHash(password);
+      mUser.setHash(newHash);
+      return true;
+    } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+      LOGGER.error("Can't hash password <" + password + ">", e.getCause());
+      return false;
+    }
+  }
 }
--- src/main/java/domain/model/Project.java
+++ src/main/java/domain/model/Project.java
@@ -7,45 +7,54 @@ import java.util.List;
 
 public class Project {
 
-    @SerializedName("_id")
-    @Expose
-    private String id;
-    @SerializedName("milestones")
-    @Expose
-    private List<Milestone> milestones = null;
+  @SerializedName("_id")
+  @Expose
+  private String id;
+  @SerializedName("milestones")
+  @Expose
+  private List<Milestone> milestones = null;
 
-    /**
-     * No args constructor for use in serialization
-     * 
-     */
-    public Project() {
-    }
+  /**
+   * No args constructor for use in serialization
+   */
+  public Project() {
+  }
 
-    /**
-     * 
-     * @param id
-     * @param milestones
-     */
-    public Project(String id, List<Milestone> milestones) {
-        super();
-        this.id = id;
-        this.milestones = milestones;
-    }
+  /**
+   * @param id
+   * @param milestones
+   */
+  public Project(String id, List<Milestone> milestones) {
+    super();
+    this.id = id;
+    this.milestones = milestones;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public void setId(String id) {
-        this.id = id;
-    }
+  public void setId(String id) {
+    this.id = id;
+  }
 
-    public List<Milestone> getMilestones() {
-        return milestones;
-    }
+  public List<Milestone> getMilestones() {
+    return milestones;
+  }
 
-    public void setMilestones(List<Milestone> milestones) {
-        this.milestones = milestones;
-    }
+  public void setMilestones(List<Milestone> milestones) {
+    this.milestones = milestones;
+  }
 
+  public boolean addMilestone(Milestone milestone) {
+    return this.milestones.add(milestone);
+  }
+
+  public boolean deleteMilestone(Milestone milestone) {
+    return this.milestones.remove(milestone);
+  }
+
+  public Milestone getMilestone(String id) {
+    return this.milestones.stream().filter(m -> m.getId().equals(id)).findFirst().orElse(null);
+  }
 }
